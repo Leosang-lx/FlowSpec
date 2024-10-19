a02915cfa1818fe4568aac099a2cc9b44013ecbe
Leosang-lx <1245977986@qq.com>
2024-10-19 17:19:50 +0800

    Complete local tensor parallelism, continue decoding with KV-cache

--- distributedTP.py
+++ distributedTP.py
@@ -1,19 +1,10 @@
 from autoregressive_inference import *
-from test_model_weight import get_transformer_model_weight
+from test_model_weight import *
 
-# split model weight for tensor parallelism
-config, tokenizer, model = load_local_pretrained_model(model_path)
-vocab_size, max_p, n_layer, d_model, h, d_h, rate = model_config = \
-        (
-            config.vocab_size, config.n_positions, config.n_layer, config.n_embd, config.n_head,
-            config.n_embd // config.n_head,
-            4)
 
-transformer_model = model.transformer
-model_weight = get_transformer_model_weight(transformer_model)
 def split_weight_TP(model_weights, heads, split_nums: int | list[int]):
     """
-    split weights
+    split weights for TP, only has the weights in **layers**
     :param model_weights:
     :param heads:
     :param split_nums: int or list(int), the heads number in each partition
@@ -57,10 +48,10 @@ def split_weight_TP(model_weights, heads, split_nums: int | list[int]):
 
         # split multi-head projection weights Wo
         attn_Wo_w, attn_Wo_b = attn_Wo_w_b
-        split_attn_Wo_ws = attn_Wo_w.split(split_embedding_num, dim=-1)
-        split_attn_Wo_bs = attn_Wo_b.split(split_embedding_num, dim=-1)
+        split_attn_Wo_ws = attn_Wo_w.split(split_embedding_num, dim=0)
+        # split_attn_Wo_bs = attn_Wo_b.split(split_embedding_num, dim=-2)
 
-        split_Wo_proj = list(zip(split_attn_Wo_ws, split_attn_Wo_bs))
+        split_Wo_proj = tuple(zip(split_attn_Wo_ws, (attn_Wo_b,) + (None,) * (split_cnt - 1)))
 
         # MLP block
         MLP_weights = layer_weights['MLP']
@@ -71,27 +62,150 @@ def split_weight_TP(model_weights, heads, split_nums: int | list[int]):
         split_lens = [sen * rate for sen in split_embedding_num]  # todo: may need another split setting, share the attn split setting first
         split_mlp1_ws = mlp1_w.split(split_lens, dim=-1)
         split_mlp1_bs = mlp1_b.split(split_lens, dim=-1)
-        split_mlp1_wb = list(zip(split_mlp1_ws, split_mlp1_bs))
+        split_mlp1_wb = tuple(zip(split_mlp1_ws, split_mlp1_bs))
 
         # split_mlp2 weights
         mlp2_w, mlp2_b = mlp2_wb
         split_mlp2_ws = mlp2_w.split(split_lens, dim=0)
-        # mlp2_b is applied for once after the results of ReduceSum, do not split
+        # mlp2_b is applied for once after the results of ReduceSum, which is applied by only one partition
+        split_mlp2_bs = (mlp2_b,) + (None,) * (split_cnt - 1)
+        split_mlp2_wb = tuple(zip(split_mlp2_ws, split_mlp2_bs))
 
         for idx_split in range(split_cnt):
             split_layer_weights = {
                 'MHA': (tuple(split_QKV_proj[idx_split]), split_Wo_proj[idx_split]),
-                'MLP': (split_mlp1_wb, split_mlp2_ws)
+                'MLP': (split_mlp1_wb[idx_split], split_mlp2_wb[idx_split])
             }
             split_layers_weights[idx_split].append(split_layer_weights)
 
     return split_layers_weights
 
 
-        # LN_weights = layer_weights['LN']
+# test TP local
+def tp_local(input_ids, split_nums: int | list[int], split_weights_tp, config, KV_cache=None):
+    """
+    :param input_ids:
+    :param split_nums: int -> number of partitions; list[int] -> number of heads in each partition
+    :param split_weights_tp:
+    :param KV_cache:
+    :return:
+    """
+    if isinstance(split_nums, int):
+        assert config.h % split_nums == 0
+        split_cnt = split_nums
+        split_nums = [config.h // split_nums] * split_cnt
+    else:
+        assert sum(split_nums) == config.h
+        split_cnt = len(split_nums)
+
+    # centralize{
+    token_embedding = transformer_model.wte(input_ids)
+    if KV_cache is None:
+        past_length = 0
+        KV_cache = tuple([[None] * n_layer] * split_num)  # distributed KVCache for TP: KV_cache[device_num][layer_idx]
+    else:
+        past_length = KV_cache[0][0].size(-2)
+    position_ids = torch.arange(past_length, input_ids.shape[-1] + past_length, dtype=torch.long, device=input_ids.device)
+    position_ids = position_ids.unsqueeze(0)
+    position_embedding = transformer_model.wpe(position_ids)
+    hidden_states = token_embedding + position_embedding
+    # }
+    # prepare split weights
+    # 先写没cache的
+    layers_weights = model_weight['layers_weights']
+    for layer_idx in range(n_layer):
+        original_layer = transformer_model.h[layer_idx]
+        correct_layer_output = original_layer(hidden_states)[0]
+
+        layer_weights = layers_weights[layer_idx]
+        split_weights_layer = [split_weights_device[layer_idx]  for split_weights_device in split_weights_tp] # 获得这层layer_idx所有的split_weights
+        ln1_w_b, ln2_w_b = layer_weights['LN']
+
+        residual = hidden_states
+        # LN1
+        hidden_states = F.layer_norm(hidden_states, (d_model,), *ln1_w_b, config.ln_eps)
+        MHA_tp_results = []
+        # MHA
+        for split_weight_layer in split_weights_layer:
+            split_MHA_weight = split_weight_layer['MHA']
+            # split_QKV_proj, split_Wo_proj = split_MHA_weight
+            split_MHA_result, layer_cache = MHA_forward_use_weights(hidden_states, split_MHA_weight, config)
+            MHA_tp_results.append(split_MHA_result)
+
+        # AllReduce
+        MHA_output = sum(MHA_tp_results)
+        # residual connection
+        hidden_states = residual + MHA_output
+
+        residual = hidden_states
+        # LN2
+        hidden_states = F.layer_norm(hidden_states, (d_model,), *ln2_w_b, config.ln_eps)
+        # MLP
+        MLP_tp_results = []
+        for split_weight_layer in split_weights_layer:
+            split_MLP_weight = split_weight_layer['MLP']
+            split_MLP_result = MLP_forward_use_weights(hidden_states, split_MLP_weight, config)
+            MLP_tp_results.append(split_MLP_result)
+
+        # AllReduce
+        MLP_output = sum(MLP_tp_results)
+        # residual connection
+        hidden_states = residual + MLP_output
+
+        # when atol=1e-7, allclose() is False
+        print(f'layer {layer_idx}', torch.allclose(hidden_states, correct_layer_output, atol=1e-6))
+
+    hidden_states = transformer_model.ln_f(hidden_states)
+
+    return hidden_states
+
+if __name__ == '__main__':
+    # split model weight for tensor parallelism
+    config, tokenizer, model = load_local_pretrained_model(model_path)
+    model.eval()
+    vocab_size, max_p, n_layer, d_model, h, d_h, rate = \
+        (
+            config.vocab_size, config.n_positions, config.n_layer, config.n_embd, config.n_head,
+            config.n_embd // config.n_head,
+            4)
+    layer_norm_eps = config.layer_norm_epsilon
+    # dropout_prob = config.resid_pdrop
+    dropout_prob = 0
+    model_config = {
+        'vocab_size': vocab_size,
+        'max_position': max_p,
+        'n_layer': n_layer,
+        'd_model': d_model,
+        'h': h,
+        'd_h': d_h,
+        'rate': rate,
+        'ln_eps': layer_norm_eps,
+        'dropout_prob': dropout_prob
+    }
+    model_config = SimpleNamespace(**model_config)
+    model_config.split_heads = 3
+
+    transformer_model = model.transformer
+    model_weight = get_transformer_model_weight(transformer_model)
+
+    text = "在一个风和日丽的下午，小镇的街道上人来人往，孩子们在巷口追逐嬉戏。李阿姨拿着刚从市场买回来的菜篮子，步履轻盈地走回家。街边的老槐树下，几位老人正围坐在一起下象棋，不时传来欢声笑语。今天是不是一个好日子？"
+    input_ids = torch.LongTensor([tokenizer.convert_tokens_to_ids(list(text))])  # .to(device)
+
+    split_num = 3
+    split_weights = split_weight_TP(model_weight, h, split_num)
+    output_tp = tp_local(input_ids, split_num, split_weights, model_config)
+    # hidden_states = transformer_model.ln_f(output_tp)
+    logits_tp = model.lm_head(output_tp)
+    from sampling import apply_sampling
+    next_token = apply_sampling(logits_tp[:, -1, :])
+    next_word = tokenizer.convert_ids_to_tokens(next_token)
+    print(next_word)
+
+    output = transformer_model(input_ids).last_hidden_state
+
+    print(torch.allclose(output, output_tp, ))
 
 
-split_weights = split_weight_TP(model_weight, h, 3)
 
-# def tensor_parallelism(split_nums):
+
 
--- test_model_weight.py
+++ test_model_weight.py
@@ -2,12 +2,12 @@
 import torch
 import torch.nn.functional as F
 import transformers.activations
-
+from types import SimpleNamespace
 from autoregressive_inference import *
 
 
 def get_transformer_model_weight(transformer_model):
-    # 根据input_ids或者input_pos查表获得embedding vector
+    # generate embeddings by lookup table
     token_embedding_weight = transformer_model.wte.weight
     position_embedding_weight = transformer_model.wpe.weight
 
@@ -57,28 +57,40 @@ def get_transformer_model_weight(transformer_model):
     return model_weight
 
 
-def split_heads(x, h, d_h):
+def split_heads(x, d_h):
+    d_model = x.size(-1)
+    assert d_model % d_h == 0
+    h = d_model // d_h
     new_shape = x.size()[:-1] + (h, d_h)
     x = x.view(new_shape)
     return x.permute(0, 2, 1, 3)  # (batch, head, seq_length, head_features)
 
 
-def merge_heads(x, h, d_h):
+def merge_heads(x, d_h):
+    h = x.size(-3)
     x = x.permute(0, 2, 1, 3).contiguous()
     new_shape = x.size()[:-2] + (h * d_h,)
     return x.view(new_shape)
 
 
 # Conv1D in GPT-2, can be replaced by nn.Linear
-def forward_Conv1D_use_weights(x, weights, output_features):
-    size_out = x.size()[:-1] + (output_features,)
+def Conv1D_forward_use_weights(x, weights):
     weight, bias = weights
+    if bias is None:
+        size_out = x.size()[:-1] + (weight.size(-1),)
+        bias = torch.zeros(x.size(-2), weight.size(-1))
+    else:
+        size_out = x.size()[:-1] + (bias.size(-1),)
     x = torch.addmm(bias, x.view(-1, x.size(-1)), weight)
     x = x.view(size_out)
     return x
 
 
 # only self attention
+# causal mask for attn
+causal_mask_cache = torch.tril(torch.ones((1024, 1024), dtype=torch.bool)).view(1, 1, 1024, 1024)
+
+
 def attn(Q, K, V, attention_mask=None, head_mask=None):
     attn_weights = torch.matmul(Q, K.transpose(-1, -2))
     # scale
@@ -109,19 +121,27 @@ def attn(Q, K, V, attention_mask=None, head_mask=None):
     return attn_output, attn_weights
 
 
-def MHA_forward_use_weights(hidden_states, MHA_weights, layer_cache=None):
+def MHA_forward_use_weights(hidden_states, MHA_weights, transformer_config, layer_cache=None):
+    """
+    :param hidden_states:
+    :param MHA_weights: can be the split_weights
+    :param transformer_config:
+    :param layer_cache:
+    :return:
+    """
     attn_proj_w_b, attn_Wo_w_b = MHA_weights
     # QKV projection
+    if len(attn_proj_w_b) == 3:  # Q_proj_w_b, K_proj_w_b, V_proj_w_b
+        attn_proj_w_b = tuple([torch.concat(ws_or_bs, dim=-1) for ws_or_bs in zip(*attn_proj_w_b)])
     attn_proj_w, attn_proj_b = attn_proj_w_b
-    QKV = forward_Conv1D_use_weights(hidden_states, attn_proj_w_b, 3 * hidden_states.size(-1))
-    # size_out = hidden_states.size()[:-1] + (3 * hidden_states.size(-1),)  # d_model = hidden_states.size(-1)
-    # QKV = torch.addmm(attn_proj_b, hidden_states.view(-1, hidden_states.size(-1)), attn_proj_w)
-    # QKV = QKV.view(size_out)
 
-    Q, K, V = QKV.split(d_model, dim=2)
-    Q = split_heads(Q, h, d_h)
-    K = split_heads(K, h, d_h)
-    V = split_heads(V, h, d_h)
+    # forward QKV projection with merged weights: x * (W_Q|W_K|W_V) = Q|K|V
+    QKV = Conv1D_forward_use_weights(hidden_states, attn_proj_w_b)
+
+    Q, K, V = QKV.split(QKV.size(-1) // 3, dim=2)
+    Q = split_heads(Q, transformer_config.d_h)
+    K = split_heads(K, transformer_config.d_h)
+    V = split_heads(V, transformer_config.d_h)
 
     if layer_cache is not None:
         # update layer_cache
@@ -133,43 +153,36 @@ def MHA_forward_use_weights(hidden_states, MHA_weights, layer_cache=None):
 
     attn_output, attn_weights = attn(Q, K, V)
 
-    attn_output = merge_heads(attn_output, h, d_h)
+    attn_output = merge_heads(attn_output, transformer_config.d_h)
     # Wo
-    attn_Wo_w, attn_Wo_b = attn_Wo_w_b
-    attn_output = forward_Conv1D_use_weights(attn_output, attn_Wo_w_b, d_model)
-    # size_out = hidden_states.size()[:-1] + (d_model,)
-    # attn_output = torch.addmm(attn_Wo_b, attn_output.view(-1, attn_output.size(-1)), attn_Wo_w)
-    # attn_output = attn_output.view(size_out)
-    attn_output = F.dropout(attn_output, dropout_prob, False, False)
+    # attn_Wo_w, attn_Wo_b = attn_Wo_w_b
+    attn_output = Conv1D_forward_use_weights(attn_output, attn_Wo_w_b)
+    attn_output = F.dropout(attn_output, transformer_config.dropout_prob, False, False)
 
     return attn_output, layer_cache_present
 
 
-def MLP_forward_use_weights(hidden_states, MLP_weights):
+def MLP_forward_use_weights(hidden_states, MLP_weights, transformer_config):
     mlp1_w_b, mlp2_w_b = MLP_weights
-    mlp1_w, mlp1_b = mlp1_w_b
-    mlp2_w, mlp2_b = mlp2_w_b
+    # mlp1_w, mlp1_b = mlp1_w_b
+    # mlp2_w, mlp2_b = mlp2_w_b
     # mlp1
-    hidden_states = forward_Conv1D_use_weights(hidden_states, mlp1_w_b, rate * d_model)
-    # size_out = hidden_states.size()[:-1] + (rate * d_model,)
-    # hidden_states = torch.addmm(mlp1_b, hidden_states.view(-1, hidden_states.size(-1)), mlp1_w)
-    # hidden_states = hidden_states.view(size_out)
+    hidden_states = Conv1D_forward_use_weights(hidden_states, mlp1_w_b)
+
     # activation
     # 暂时只考虑了GPT-2用的new gelu
     hidden_states = transformers.activations.NewGELUActivation().forward(hidden_states)
     # hidden_states = act(hidden_states)
     # mlp2
-    hidden_states = forward_Conv1D_use_weights(hidden_states, mlp2_w_b, d_model)
-    # size_out = hidden_states.size()[:-1] + (d_model,)
-    # hidden_states = torch.addmm(mlp2_b, hidden_states.view(-1, hidden_states.size(-1)), mlp2_w)
-    # hidden_states = hidden_states.view(size_out)
+    hidden_states = Conv1D_forward_use_weights(hidden_states, mlp2_w_b)
+
     # dropout
-    hidden_states = F.dropout(hidden_states, dropout_prob, False, False)
+    hidden_states = F.dropout(hidden_states, transformer_config.dropout_prob, False, False)
 
     return hidden_states
 
 
-def forward_layer_use_weights(hidden_states, layer_weights, layer_cache=None):
+def layer_forward_use_weights(hidden_states, layer_weights, transformer_config, layer_cache=None):
     attn_proj_w_b, attn_Wo_w_b = layer_weights['MHA']
     mlp1_w_b, mlp2_w_b = layer_weights['MLP']
     ln1_w_b, ln2_w_b = layer_weights['LN']
@@ -178,8 +191,7 @@ def forward_layer_use_weights(hidden_states, layer_weights, layer_cache=None):
     # LN1
     hidden_states = F.layer_norm(hidden_states, (d_model,), *ln1_w_b, layer_norm_eps)
     # MHA
-    attn_output, layer_cache_present = MHA_forward_use_weights(hidden_states, (attn_proj_w_b, attn_Wo_w_b),
-                                                               layer_cache)
+    attn_output, layer_cache_present = MHA_forward_use_weights(hidden_states, (attn_proj_w_b, attn_Wo_w_b), transformer_config, layer_cache)
     # residual connection
     hidden_states = residual + attn_output
 
@@ -187,18 +199,19 @@ def forward_layer_use_weights(hidden_states, layer_weights, layer_cache=None):
     # LN2
     hidden_states = F.layer_norm(hidden_states, (d_model,), *ln2_w_b, layer_norm_eps)
     # MLP
-    hidden_states = MLP_forward_use_weights(hidden_states, (mlp1_w_b, mlp2_w_b))
+    hidden_states = MLP_forward_use_weights(hidden_states, (mlp1_w_b, mlp2_w_b), transformer_config)
     hidden_states = residual + hidden_states
 
     return hidden_states
 
 
 # output use model weights
-def forward_use_weights(input_ids, model_weights, KV_cache=None, device='cpu'):
+def model_forward_use_weights(input_ids, model_weights, transformer_config, KV_cache=None, device='cpu'):
     # token_embedding_weight, position_embedding_weight = model_weights['embedding_weights']
     layers_weight = model_weights['layers_weights']
     # lm_head_weight = model_weights['lm_head_weights']
 
+    # embedding
     token_embedding = transformer_model.wte(input_ids)
     if KV_cache is None:
         past_length = 0
@@ -211,30 +224,9 @@ def forward_use_weights(input_ids, model_weights, KV_cache=None, device='cpu'):
     hidden_states = token_embedding + position_embedding
 
     for layer_weight, layer_cache in zip(layers_weight, KV_cache):
-        hidden_states = forward_layer_use_weights(hidden_states, layer_weight, layer_cache)
-        # attn_proj_w_b, attn_Wo_w_b = layer_weight['MHA']
-        # mlp1_w_b, mlp2_w_b = layer_weight['MLP']
-        # ln1_w_b, ln2_w_b = layer_weight['LN']
-        #
-        # residual = hidden_states
-        # # LN1
-        # hidden_states = F.layer_norm(hidden_states, (d_model,), *ln1_w_b, layer_norm_eps)
-        # # MHA
-        # attn_output, layer_cache_present = MHA_forward_use_weights(hidden_states, (attn_proj_w_b, attn_Wo_w_b),
-        #                                                            layer_cache)
-        #
-        # # residual connection
-        # hidden_states = residual + attn_output
-        #
-        # residual = hidden_states
-        # # LN2
-        # hidden_states = F.layer_norm(hidden_states, (d_model,), *ln2_w_b, layer_norm_eps)
-        # # MLP
-        # hidden_states = MLP_forward_use_weights(hidden_states, (mlp1_w_b, mlp2_w_b))
-        #
-        # # residual connection
-        # hidden_states = residual + hidden_states
-    hidden_states = transformer_model.ln_f(hidden_states)
+        hidden_states = layer_forward_use_weights(hidden_states, layer_weight, transformer_config, layer_cache)
+
+    hidden_states = transformer_model.ln_f(hidden_states)  # model finally has a LayerNorm
 
     return hidden_states
 
@@ -251,7 +243,7 @@ if __name__ == '__main__':
 
     # config
     input_length = 100
-    vocab_size, max_p, n_layer, d_model, h, d_h, rate = model_config = \
+    vocab_size, max_p, n_layer, d_model, h, d_h, rate = \
         (
             config.vocab_size, config.n_positions, config.n_layer, config.n_embd, config.n_head,
             config.n_embd // config.n_head,
@@ -259,9 +251,18 @@ if __name__ == '__main__':
     layer_norm_eps = config.layer_norm_epsilon
     # dropout_prob = config.resid_pdrop
     dropout_prob = 0
-
-    # causal mask for attn
-    causal_mask_cache = torch.tril(torch.ones((max_p, max_p), dtype=torch.bool)).view(1, 1, max_p, max_p)
+    model_config = {
+        'vocab_size': vocab_size,
+        'max_position': max_p,
+        'n_layer': n_layer,
+        'd_model': d_model,
+        'h': h,
+        'd_h': d_h,
+        'rate': rate,
+        'ln_eps': layer_norm_eps,
+        'dropout_prob': dropout_prob
+    }
+    model_config = SimpleNamespace(**model_config)
 
     casual_input = torch.randn(1, 100, d_model)
 
@@ -269,23 +270,24 @@ if __name__ == '__main__':
     # ln1_1 = transformer_model.h[0].ln_1
     # output = ln1_1(casual_input)
     # ln1_1_weights = model_weight['layers_weights'][0]['LN'][0]
-    # output2 = F.layer_norm(casual_input, (d_model,), *ln1_1_weights, eps=layer_norm_eps)
-    # print(torch.equal(output, output2))
+    # output2 = F.layer_norm(casual_input, (d_model,), *ln1_1_weights, eps=model_config.ln_eps)
+    # print('Test LayerNorm:', torch.equal(output, output2))
 
     # test attn: pass
     # attn1 = transformer_model.h[0].attn
     # output = attn1(casual_input)[0]
     # attn1_weights = model_weight['layers_weights'][0]['MHA']
-    # output2 = MHA_forward_use_weights(casual_input, attn1_weights)[0]
-    # print(torch.allclose(output, output2))
+    # output2 = MHA_forward_use_weights(casual_input, attn1_weights, model_config)[0]
+    # print('Test MHA:', torch.allclose(output, output2))
 
     # test layer input: pass
-    first_layer = transformer_model.h[0]
-    first_layer_output = first_layer(casual_input)[0]
-    first_layer_weight = model_weight['layers_weights'][0]
-    my_first_layer_output = forward_layer_use_weights(casual_input, first_layer_weight)
-    print(torch.equal(first_layer_output, my_first_layer_output))
+    # first_layer = transformer_model.h[0]
+    # first_layer_output = first_layer(casual_input)[0]
+    # first_layer_weight = model_weight['layers_weights'][0]
+    # my_first_layer_output = forward_layer_use_weights(casual_input, first_layer_weight)
+    # print('Test Layer:', torch.equal(first_layer_output, my_first_layer_output))
 
+    # test model forward: pass
     text = "在一个风和日丽的下午，小镇的街道上人来人往，孩子们在巷口追逐嬉戏。李阿姨拿着刚从市场买回来的菜篮子，步履轻盈地走回家。街边的老槐树下，几位老人正围坐在一起下象棋，不时传来欢声笑语。今天是不是一个好日子？"
     # try inference with weights only
     test_input = torch.LongTensor([tokenizer.convert_tokens_to_ids(list(text))])  # .to(device)
@@ -294,8 +296,8 @@ if __name__ == '__main__':
     with torch.no_grad():
         output_logits = model(test_input).logits
 
-    hidden_states = forward_use_weights(test_input, model_weight)
+    hidden_states = model_forward_use_weights(test_input, model_weight, model_config)
     logits = model.lm_head(hidden_states)
 
     assert logits.shape == output_logits.shape
-    print(torch.allclose(logits, output_logits))
+    print('Test Model:', torch.equal(logits, output_logits))
