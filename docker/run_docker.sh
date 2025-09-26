3f44bfb994c877a35b5b9ed1ae771b196069516a
Leosang-lx <1245977986@qq.com>
2025-09-26 08:16:05 +0000

    minor changes

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,15 +1,19 @@
 # !/bin/bash
 
 ### build docker image
-### docker build -t test_dict .
+### docker build -t test_dist .
+
+### continue
+### docker exec -it contain_name bash
 
 ### run docker image
+#   -u $(id -u):$(id -g) \
+#   -p 12345:12345 \
 docker run -it \
   --gpus all \
   -v $HOME/big_file:/model_file \
   -v $HOME/project:/project \
-  -u $(id -u):$(id -g) \
-  -p 12345:12345 \
   --name lx_test \
+  --network host \
   test_dist \
   bash
